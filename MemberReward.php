4abc327bf65cb4ae9d760ead975403ba9c9bb7f6
Adhiska Putri Maharani <adhiskaputri25@gmail.com>
2016-10-04 08:33:37 +0700

    view controller

---
+++ MemberReward.php
@@ -0,0 +1,15 @@
+
+application/view/
+application/view/MemberReward.php
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Aisindo | kpplD.com</title>
+</head>
+<body>
+	<h1>Selamat Datang Di Member Reward Aisindo</h1>
+	
+	<h3>log in</h3>
+</body>
+</html>
