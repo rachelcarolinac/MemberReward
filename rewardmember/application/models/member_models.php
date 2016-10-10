e11e9aec69a462dc656710ea8f311baa2fddaeab
rachel <rachelyifan57@gmail.com>
2016-10-10 22:11:24 +0700

    member pages

---
+++ rewardmember/application/models/member_models.php
@@ -0,0 +1,12 @@
+<?php 
+class member_models extends CI_model{
+	
+	public function __construct(){
+		$this->load->database();
+	}
+
+	public function keaktifan($memberid, $jenis_acara, $nama_acara, $sertifikat){
+		
+	}
+}
+ ?>
---
+++ rewardmember/application/views/member/formkeaktifan.php
@@ -0,0 +1,32 @@
+
+<body>
+
+	<h1> Form Keaktifan Member</h1>
+	
+
+
+<form>
+
+
+<div>
+<label> member ID:
+</label>
+<input type="text" id="memberid" name="memberid">
+</div>
+<div>
+<label> Jenis Acara:
+</label>
+<input type="text" >
+</div>
+<div>
+<label> nama Acara:
+</label>
+<input type="text" >
+</div>
+<div>
+<label> Upload Sertifikat:
+</label>
+<input type="file" >
+</div>
+
+</form>
