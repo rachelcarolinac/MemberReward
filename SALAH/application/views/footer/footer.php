e29d70b742ad59a3f1e7ec5bbfc17faf9469c993
Adhiska Putri Maharani <adhiskaputri@gmail.com>
2016-11-15 17:08:56 +0700

    New Version

--- SALAH/application/views/footer/footer.php
+++
@@ -1,9 +0,0 @@
- <footer>
-        
-        <div class="footer-bottom">
-            <p>&copy; YourSite 2013. <a href="http://zypopwebtemplates.com/">Free Website Templates</a> by ZyPOP</p>
-         </div>
-    </footer>
-</div>
-</body>
-</html>
