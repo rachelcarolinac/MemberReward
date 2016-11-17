d591a938a155f6b4b15cd5289230d43dc9f72c0a
rachelcarolinac <rachelcarolinac@gmail.com>
2016-11-17 23:51:54 +0700

    Add files via upload
    view daftar admin

---
+++ daftar.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<link href="<?php echo base_url('assets/plugin/datatables-plugins/dataTables.bootstrap.css'); ?>" rel="stylesheet">
+<link href="<?php echo base_url('assets/plugin/datatables-responsive/dataTables.responsive.css'); ?>" rel="stylesheet">
+
+<div id="page-wrapper">
+	
+	<div class="row">
+		<div class="col-lg-12">
+			<h1 class="page-header">Daftar Keaktifan</h1>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-lg-12">
+			<div class="panel panel-default">
+
+				<div class="panel-heading">
+					Daftar keaktifan pengguna
+				</div>
+
+				<div class="panel-body">
+					<div class="row">
+						<div class="col-lg-12">
+							<table width="100%" class="table table-striped table-bordered table-hover" id="daftar_keaktifan">
+								
