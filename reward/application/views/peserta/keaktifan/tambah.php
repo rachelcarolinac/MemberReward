458ad887d8ec5b666b9cf3a131735cc3f8808144
Febe Panjaitan <febeslp@gmail.com>
2016-11-17 23:37:52 +0700

    Update View Febe

--- reward/application/views/peserta/keaktifan/tambah.php
+++ reward/application/views/peserta/keaktifan/tambah.php
@@ -21,7 +21,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					<div class="row">
 						<div class="col-lg-12">
 						<?php if($alert!=null){echo $alert;} ?>
-							
+						<form action="" method="POST" accept-charset="utf-8" enctype="multipart/form-data">
+								<div class="form-group">
+									<label>Pilih Kegiatan :</label>
+									<select class="form-control" name="kegiatan" required="">
+										<option value="" disabled selected>Pilih salah satu kegiatan</option>
+										<?php foreach ($daftar_kegiatan as $dk) : ?>
+											<option value="<?php echo $dk->id_kegiatan; ?>">
+												<?php echo $dk->nama_kegiatan.' - '.$dk->poin_kegiatan; ?>
+											</option>
+										<?php endforeach; ?>
+									</select>
+								</div>
+								<div class="form-group">
+									<label>Deskripsi</label>
+									<input class="form-control" name="deskripsi" placeholder="Deskripsi Kegiatan yang Anda Ikuti" required>
+								</div>
+								<div class="form-group">
+									<label>Unggah Bukti Kegiatan</label>
+									<input type="file" name="bukti" accept="image/*" required />
+								</div>
+								<button class="btn btn-info" name="submit">Submit</button>
+							</form>	
 						</div>
 					</div>
 				</div>
--- reward/application/views/peserta/keaktifan/ubah.php
+++ reward/application/views/peserta/keaktifan/ubah.php
@@ -42,7 +42,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 										Lihat
 									</button>
 
-								
+								<div class="modal fade" id="buktiKegiatan" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+										<div class="modal-dialog modal-lg" role="document">
+											<div class="modal-content">
+												<div class="modal-header">
+													<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+													<h4 class="modal-title" id="myModalLabel">Bukti Kegiatan</h4>
+												</div>
+												<div class="modal-body">
+													<img src="<?php echo base_url('uploads/'.$rk->nama_file); ?>" alt="" width=100%>
+												</div>
+												<div class="modal-footer">
+													<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
 												</div>
 											</div>
 										</div>
