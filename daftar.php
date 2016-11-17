71ea2f1d50b04ba615f8e3b8f3bc2377e28a349e
rachelcarolinac <rachelcarolinac@gmail.com>
2016-11-17 23:53:21 +0700

    Add files via upload
    update view daftar keaktifan

--- daftar.php
+++ daftar.php
@@ -25,3 +25,45 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 						<div class="col-lg-12">
 							<table width="100%" class="table table-striped table-bordered table-hover" id="daftar_keaktifan">
 								
+<thead>
+									<tr>
+										<th>Kegiatan</th>
+										<th>Deskripsi Kegiatan</th>
+										<th>Poin</th>
+										<th>Bukti</th>
+										<th>Status</th>
+									</tr>
+								</thead>
+								<?php foreach($daftar_keaktifan as $dk): ?>
+									<tbody>
+										<tr class="odd gradeX">
+											<td><a href="<?php echo site_url('Admin/Keaktifan/Rincian/'.$dk->id_keaktifan); ?>"><?php echo $dk->nama_kegiatan; ?></a></td>
+											<td><?php echo $dk->deskripsi; ?></td>
+											<td><?php echo $dk->poin_kegiatan; ?></td>
+											<td><a href="<?php echo base_url('uploads/'.$dk->nama_file); ?>">Lihat</a></td>
+											<td><?php echo ($dk->status_keaktifan==0?'Belum Disetujui':($dk->status_keaktifan==2?'Ditolak':'Disetujui')); ?></td>
+										</tr>
+									</tbody>
+								<?php endforeach; ?>
+							</table>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+</div>
+
+<script src="<?php echo base_url('assets/plugin/datatables/js/jquery.dataTables.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/plugin/datatables-plugins/dataTables.bootstrap.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/plugin/datatables-responsive/dataTables.responsive.js'); ?>"></script>
+
+<script>
+	$(document).ready(function() {
+		$('#daftar_keaktifan').DataTable({
+			responsive: true
+		});
+	});
+</script>
