eb9dbedf35fdf9e1a4cbcf8482e472e774202b3e
rachelcarolinac <rachelcarolinac@gmail.com>
2016-11-17 19:08:55 -0800

    Add files via upload
    tambah fungsi

--- reward/application/models/Keaktifan_model.php
+++ reward/application/models/Keaktifan_model.php
@@ -24,7 +24,12 @@ class Keaktifan_model extends CI_Model {
 		return $this->db->affected_rows();
 	}
 
-	
+	public function ubah($id,$data){
+		$this->db->where('id_keaktifan',$id);
+		$this->db->update('keaktifan',$data);
+		return $this->db->affected_rows();
+	}
+
 	public function hapus($id){
 		$this->db->where('id_keaktifan',$id);
 		$this->db->delete('keaktifan');
