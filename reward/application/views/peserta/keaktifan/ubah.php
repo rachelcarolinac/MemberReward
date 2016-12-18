f49a2280aa1a5530fd3c7823acc78de54ec12fbc
Febe Panjaitan <febeslp@gmail.com>
2016-12-18 21:57:24 +0700

    ubah.php
    Update view hapus pada ubah.php

--- reward/application/views/peserta/keaktifan/ubah.php
+++ reward/application/views/peserta/keaktifan/ubah.php
@@ -42,7 +42,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 										Lihat
 									</button>
 
-								<div class="modal fade" id="buktiKegiatan" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+									<div class="modal fade" id="buktiKegiatan" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
 										<div class="modal-dialog modal-lg" role="document">
 											<div class="modal-content">
 												<div class="modal-header">
